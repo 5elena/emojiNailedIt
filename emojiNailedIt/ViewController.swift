a285b47d08f2cb6cefa940f03b05be81b6fad78b
Selena <selena@2001-b011-400a-10cb-1017-8985-9cf3-55a1.dynamic-ip6.hinet.net>
2018-08-19 18:54:33 +0800

    emoji say it!

--- emojiNailedIt/ViewController.swift
+++ emojiNailedIt/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //Emoji靠近彼此
+    @IBOutlet weak var getCloserSlider: UISlider!
+    @IBOutlet weak var smileEmoji: UIImageView!
+    @IBOutlet weak var loveEmoji: UIImageView!
+    
+    //SpeechVoice
+    @IBOutlet weak var saySomething: UITextField!
+    @IBOutlet weak var speedRate: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func sliderChange(_ sender: UISlider) {
+        loveEmoji.frame.origin.x = CGFloat(30+9*sender.value)
+        smileEmoji.frame.origin.x = CGFloat(500-9*sender.value)
+    }
 
+    //Speech Rate Slider Change 告白吧 emoji
+
+    @IBAction func speechRateChange(_ sender: Any) {
+        saySomething.text = String(speedRate.value)
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        let speechUtterance = AVSpeechUtterance(string: saySomething.text!)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-tw")
+        let synthesizer = AVSpeechSynthesizer()
+        speechUtterance.rate = speedRate.value*0.05
+        synthesizer.speak(speechUtterance)
+    }
+    
 }
 
